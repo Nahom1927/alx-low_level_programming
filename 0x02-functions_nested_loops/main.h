f9c894f3b11adf3b0afd60d3044a7d025c79edee
Nahom1927 <yheys898184@gmail.com>
2022-09-13 22:56:54 +0300

    main.h0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+void print_alphabet(void);
+print_alphabet_x10(void);
+int is lower(int c);
+int _putchar(char c);
+int _is alpha(int n);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
