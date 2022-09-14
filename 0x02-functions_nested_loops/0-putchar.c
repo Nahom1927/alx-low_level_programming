2115e28ee00bc53f8b920a30b0e47792187d4d27
Nahom1927 <yheys898184@gmail.com>
2022-09-14 03:05:01 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include “main.h"
+
+   #include “main.h"
 
 /**
   * main - Entry point
@@ -7,16 +8,14 @@
   */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
-
+	_putchar(‘_');
+	_putchar(‘p');
+	_putchar(‘u');
+	_putchar(’t');
+	_putchar(‘c');
+	_putchar(‘h');
+	_putchar(‘a');
+	_putchar(‘r’);
+        _putchar('\n');
 	return (0);
 }
