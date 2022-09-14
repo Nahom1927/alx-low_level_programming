7ecbac5d75e7f31fa72361484b087a723a0cdb5b
Nahom1927 <yheys898184@gmail.com>
2022-09-14 11:02:26 +0300

    0 Task

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void postitive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_days(int month, int day);
+void print_time_table(int n);
+
+#endif
