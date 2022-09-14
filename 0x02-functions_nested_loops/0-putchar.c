49a4850fa00a8b9156f4098caee9e8464343da62
Nahom1927 <yheys898184@gmail.com>
2022-09-14 09:58:03 +0300

    hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,32 @@
-#include “main.h"
+#include <unistd.h>
+#include "main.h"
+
 /**
-  * main - Entry point
-  *
-  * Return: Always 0
-  */
-int main(void)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void){
+
+          char ch[8] = "_putchar";
+                 int i = 0;
+                 char c;
+             
+               while (i<=8){
+                       c = ch[i];
+                         _putchar(c);
+                       i++;
+
+
+     }
+           _putchar('\n');
+              return(0);
+
+
+}
+int _putchar(char c)
 {
-	_putchar(‘_');
-	_putchar(‘p');
-	_putchar(‘u');
-	_putchar(’t');
-	_putchar(‘c');
-	_putchar(‘h');
-	_putchar(‘a');
-	_putchar(‘r’);
-        _putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,1 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDE
-
-void print_alphabet(void);
-print_alphabet_x10(void);
-int is lower(int c);
 int _putchar(char c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
-void print_times_table(int n);
-Void num (int r ,int n );
-
-#endif
