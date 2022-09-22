3b0c977685c3f789eb6c731491eacb8119da123f
Nahom1927 <yheys898184@gmail.com>
2022-09-21 19:36:03 -0700

    0-strcat.c1-strncat.c 2-strncpy.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,13 +5,13 @@
   *@src: string to be completed at end of dest
   *@n:integer parameter to compare index to
   *Return: returns new concatenated string
-  * /
+  */
   char *_strncat(char *dest, char *src, int n)
   {
   int index =0, dest _len = 0;
-  while (dest[index++])
-  dest_len++;
-  for (index = 0; src[index] && index < n; index++)
-  dest[dest_len++] = src[index];
-  return (dest);
+         while (dest[index++])
+                dest_len++;
+        for (index = 0; src[index] && index < n; index++)
+               dest[dest_len++] = src[index];
+          return (dest);
   }
