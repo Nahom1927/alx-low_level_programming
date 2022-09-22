6bc90b3689f8f76059cfd47f68523fda83383458
Nahom1927 <yheys898184@gmail.com>
2022-09-22 12:32:14 +0300

    print remaining days.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,10 +2,5 @@
 #define MAIN_H
 
 #include <stdio.h>
-
 void positive_or_negative(int i);
-Int largest_number(int, int, int);
-Void print_remaining_days(int, int, int);
-Int convert_day(int, int);
-
-#endif /* MAIN_H */
+#endif
