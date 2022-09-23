2b7d2214c191ad850bdb048929e37bb282ab2d3b
Nahom1927 <yheys898184@gmail.com>
2022-09-23 13:21:44 +0300

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,61 @@
-# include   " main.h " 
-  
-  /* * 
-    * main - Test function for positive or negative 
-    * Return: 0 
-     */ 
-  
-  int   main ( void ) 
-  { 
-           int  i; 
-  
-          i =  0 ; 
-           positive_or_negative (i); 
-  
-  return  ( 0 ); 
-  }
+#include "main.h"
+
+/**
+ * _strncat - a function that concatenates two strings
+ *
+ * @dest: pointer
+ * @src: pointer
+ * @n: parameter
+ *
+ * Return: character
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
+
+
+
+
+
+
+
+#include "main.h"
+
+/**
+ * _strncat - a function that concatenates two strings
+ *
+ * @dest: pointer
+ * @src: pointer
+ * @n: parameter
+ *
+ * Return: character
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
+
+
