9007ee7719a37045ec7a364931bca6d4d492b366
Nahom1927 <yheys898184@gmail.com>
2022-12-14 11:05:36 -0800

    all

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-gcc -fPIC -c *.c
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
-export LD_LIBRARY_PATH=.:SLD_LIBRARY_PATH
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
